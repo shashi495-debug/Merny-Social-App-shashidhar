16d1cac855e76d0b88b97829bf430685ef015776
shashidhar reddy <113178949+shashi495-debug@users.noreply.github.com>
2023-12-28 16:07:54 +0530

    first commit

---
+++ config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url : 'mongodb+srv://shashidhar495:shashi495@cluster0.b6twmit.mongodb.net/'
+};
+
---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+/* Verify Token function code start here */
+function verifyToken(req, res, next) {
+  const token = req.headers["x-auth-token"];
+  if (!token) {
+    return res.status(400).send({
+      msg: "Invalid Authentication",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.jwtPrivateKey);
+    req._id = decoded._id;
+    next();
+  } catch (err) {
+    return res.status(400).send({
+      msg: "Invalid Token",
+    });
+  }
+}
+
+
+module.exports={
+    verifyToken:verifyToken, 
+}
+
